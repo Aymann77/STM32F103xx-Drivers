339f21ee0bfffc0e984242d9a2ff0fdf8aa46648
EngMohammed-Ayman <engmohammedaymann@gmail.com>
2023-08-19 18:28:27 +0300

    USART V1.0 Added with Basic Functionalities Provided

---
+++ MCAL/USART/Inc/USART_Config.h
@@ -0,0 +1,24 @@
+/*
+ ******************************************************************************
+ * @file           : USART_Config.h
+ * @Author         : Mohammed Ayman Shalaby
+ * @brief          : Header file for USART_Config.c file
+ * @Date           : Aug 19, 2023
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Ayman.
+ * All rights reserved.
+ *
+ ******************************************************************************
+ */
+#ifndef USART_INC_USART_CONFIG_H_
+#define USART_INC_USART_CONFIG_H_
+
+/* ======================================================================
+ * CONFIGURATION PARAMETERS
+ * ====================================================================== */
+
+#define F_PCLK 8000000UL
+
+#endif /* USART_INC_USART_CONFIG_H_ */
